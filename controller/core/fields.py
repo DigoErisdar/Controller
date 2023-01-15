bb058b4d104ae5133098468f2df3e94785c3204f
Digo Erisdar <kingofthedemons@mail.ru>
2023-01-15 18:00:59 +0300

    append format date field

--- controller/core/fields.py
+++ controller/core/fields.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from typing import List
+
 from pydantic import BaseModel
 
 
@@ -20,3 +23,23 @@ class Const(CustomField):
     @classmethod
     def validate(cls, value, values, config, field):
         return field.default
+
+
+class FormatDate(CustomField):
+    """Поле для форматирования даты из строки"""
+    _input_format: List[str]
+    _output_format: str
+
+    @classmethod
+    def validate(cls, value, values, config, field):
+        for frmt in cls._input_format:
+            try:
+                dt = datetime.strptime(value, frmt)
+            except Exception as e:
+                pass
+            else:
+                try:
+                    return dt.strftime(cls._output_format)
+                except Exception:
+                    raise ValueError("Получаемая дата не совпадает не подходит под нужный формат")
+        raise ValueError("Не удалось получить дату дату")
