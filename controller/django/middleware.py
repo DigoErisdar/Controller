38393972cf1816390cbf73fafc3b150de8478604
Галкин Даниил <51191928+DigoErisdar@users.noreply.github.com>
2023-03-29 15:32:07 +0300

    Update middleware.py

--- controller/django/middleware.py
+++ controller/django/middleware.py
@@ -7,6 +7,6 @@ class ErrorMiddleware(MiddlewareMixin):
 
     def process_exception(self, request, exception):
         if isinstance(exception, ValidationError):
-            return JsonResponse({'errors': exception.errors()})
+            return JsonResponse({'errors': exception.errors()}, status=500)
         else:
             raise exception
